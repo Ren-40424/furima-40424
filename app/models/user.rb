1bfb22e7f32c1da7b636bf40902d1b6fb97520cd
Ren-40424 <takashimaren0527@gmail.com>
2024-04-09 23:59:07 +0900

    userモデルに正規表現によるバリデーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ApplicationRecord
   validates :read_first_name, presence: true
   validates :read_last_name,  presence: true
   validates :birth,           presence: true
+
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+  }
+
+  validates :first_name, :last_name, format: {
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  }
+
+  validates :read_first_name, :read_last_name, format: {
+    with: /\A[ァ-ヶー]+\z/
+  }
+
 end
