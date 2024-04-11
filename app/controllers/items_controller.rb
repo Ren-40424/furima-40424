22123124b30b6c962da6128de03a1b6e468eed61
Ren-40424 <takashimaren0527@gmail.com>
2024-04-12 00:17:22 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,24 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def move_to_sign_in
     unless user_signed_in?
       redirect_to user_session_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :price, :category_id, :condition_id, :shipping_payer_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_payer
   belongs_to :prefecture
   belongs_to :shipping_date
+  has_one_attached :image
 
   validates :name,              presence: true
   validates :explanation,       presence: true
-  validates :price,             presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price,             presence: true
+  validates :price,             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, allow_nil: true
   validates :category_id,       numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_payer_id, numericality: { other_than: 1, message: "can't be blank" }
