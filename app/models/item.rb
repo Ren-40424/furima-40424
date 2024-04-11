2fb7341c38bb69946b6aec7bebf7b8cf3d028cfa
Ren-40424 <takashimaren0527@gmail.com>
2024-04-11 23:15:27 +0900

    価格制限のバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
   validates :name,              presence: true
   validates :explanation,       presence: true
-  validates :price,             presence: true
+  validates :price,             presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :category_id,       numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_payer_id, numericality: { other_than: 1, message: "can't be blank" }
