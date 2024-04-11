c524fd7b6f851b0806ed7df4015b02c802737342
Ren-40424 <takashimaren0527@gmail.com>
2024-04-11 21:51:16 +0900

    ログアウト状態でのリダイレクトを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, except: :index
+  
   def index
   end
 
   def new
+  end
+
+  private
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 end
