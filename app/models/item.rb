1176442be67f665a7e0824ba90bc531ef3f3dc73
Ren-40424 <takashimaren0527@gmail.com>
2024-04-13 23:03:01 +0900

    usersテーブルとitemsテーブルにアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_payer
@@ -17,5 +18,4 @@ class Item < ApplicationRecord
   validates :shipping_payer_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id,  numericality: { other_than: 1, message: "can't be blank" }
-
 end
