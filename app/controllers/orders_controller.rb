73fffd2b0353922edd26781967b2ad7682850e83
Ren-40424 <takashimaren0527@gmail.com>
2024-04-23 23:44:31 +0900

    Formオブジェクトを用いた購入情報の登録機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
+    @order_destination = OrderDestination.new
+  end
+
+  def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id)
   end
 end
